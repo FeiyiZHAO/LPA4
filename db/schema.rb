d1f63f3264d7905264517ad3498c4dcfe49223b8
Feiyi Zhao <fyzhao>
2019-10-01 16:26:09 +0200

    add code for students

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_10_01_113404) do
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "last_name"
+    t.string "birthday"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
